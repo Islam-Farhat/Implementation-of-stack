896079d0d9bed43c0996b3e847306deb69f56d91
Islam-Farhat <islamfarhat100@gmail.com>
2022-01-18 16:18:45 +0200

    Satck Implementation first version

---
+++ Stack-Implementation/Stack-Implementation/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack_Implementation
+{
+    class LinkedlistSatck<t>
+    {
+        LinkedList<t> list;
+        public LinkedlistSatck()
+        {
+            list = new LinkedList<t>();
+        }
+        public void Push(t item)
+        {
+            list.AddLast(item);
+        }
+        public t Pop()
+        {
+            t item = list.Last.Value;
+            list.RemoveLast();
+            return item;
+        }
+
+        public t Peek()
+        {
+            return list.Last.Value;
+        }
+        public int Count { get { return list.Count; } }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedlistSatck<int> list = new LinkedlistSatck<int>();
+            list.Push(100);
+            list.Push(200);
+            list.Push(300);
+            list.Push(400);
+
+            
+            Console.WriteLine($"Count: {list.Count}");
+            while (list.Count>0)
+            {
+                Console.WriteLine(list.Pop());
+            }
+
+        }
+    }
+}
